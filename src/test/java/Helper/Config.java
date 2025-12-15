94f4df2dcbb17809910505dd434e847e0add495d
brahimbounguicha <brahim.bounguicha@esprit.tn>
2025-12-15 20:37:00 +0100

    sole change

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -5,6 +5,10 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 
 public class Config {
+	
+	// je change le Webdrier 
+	
+	public static  WebDriver driver2;
 
 	public static WebDriver driver; 
 	
